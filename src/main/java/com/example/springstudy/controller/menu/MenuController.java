69933b7bb7feb4f8842c1d717835f2b4e2f8063e
hwlee0103 <sdiverHyeWon0103@gmail.com>
2022-11-21 21:51:18 +0900

    add menu detail html + menu controller memo

--- src/main/java/com/example/springstudy/controller/menu/MenuController.java
+++ src/main/java/com/example/springstudy/controller/menu/MenuController.java
@@ -9,14 +9,24 @@ import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping(value = "/menu")
-// Q. menu는 항상 보여야하는거면 어떻게 항상 보여주지?
 public class MenuController {
 
 
     //#region - 조회
+
+    //Q. menu는 항상 보여야하는거면 어떻게 항상 보여주지?
+
+    //list도 조회해서 보여줘야하는건가?
+    //list에는 Deth별로 디자인을 바꿔서 보여줘야할 필요가?
+
+    //메뉴 detail
+
     //#endregion
 
     //#region - 생성
+
+    //menu를 생성할 땐, 어떤걸 기준으로 depth나 parent 를 설정해야하지?
+
 
     // 메뉴 생성
 
